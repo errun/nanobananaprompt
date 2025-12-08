de10a198e10e2d4793a8e42ebc5d6262d8878673
errun <zhangwei@minimax.ltd>
2025-12-08 17:14:04 +0800

    Add Clarity script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -53,6 +53,17 @@ export default function RootLayout({
             gtag('config', 'G-MBR7WGKYYW');
           `}
         </Script>
+
+        {/* Microsoft Clarity */}
+        <Script id="ms-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "ui8icv7enk");
+          `}
+        </Script>
       </body>
     </html>
   );
